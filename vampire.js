abde11e9f739987644b16f7e32792cfc4c3c8a19
alex-hladun <alexhladun@gmail.com>
2020-06-11 14:39:52 +0000

    Updated methods in vampire.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numOfVamps = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numOfVamps ++;
+    }
+    return numOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
   }
 
   /** Stretch **/
@@ -37,6 +45,27 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    let vamp1Array = [this];
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      vamp1Array.push(currentVamp);
+    }
+
+    currentVamp = vampire;
+    let vamp2Array = [vampire];
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      vamp2Array.push(currentVamp);
+    }
+
+    for (const vamp of vamp1Array) {
+      for (const vamp2 of vamp2Array) {
+        if (vamp === vamp2) {
+          return vamp;
+        }
+      }
+    }
   }
 }
 
